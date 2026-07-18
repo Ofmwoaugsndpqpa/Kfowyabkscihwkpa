8b477f270d59e6c81dad2c0cfa39084faf420d45
siosio12921-afk <siosio12921@gmail.com>
2026-07-19 04:08:52 +0700

    Sion Auto Regen HP.lua

--- Sion Auto Regen HP.lua
+++ Sion Auto Regen HP.lua
@@ -4,56 +4,16 @@ script_author("Sion")
 require "lib.moonloader"
 require "lib.sampfuncs"
 local sampev = require "lib.samp.events"
-local lastMsg = 0
+
 local enabled = false
-
-local lockFile = getWorkingDirectory() .. "/lib/luac.dll"
-
-local expireTime = os.time({
-    year = 2026,
-    month = 7,
-    day = 22,
-    hour = 00,
-    min = 0,
-    sec = 0
-})
-
-local function saveLockData(lastTime, isLocked)
-    local file = io.open(lockFile, "w")
-    if file then
-        file:write(tostring(lastTime) .. "|" .. tostring(isLocked))
-        file:close()
-    end
-end
-
-local function loadLockData()
-    local file = io.open(lockFile, "r")
-    if not file then
-        return 0, false
-    end
-
-    local data = file:read("*a")
-    file:close()
-
-    local lastTime, locked = data:match("([^|]+)|([^|]+)")
-
-    return tonumber(lastTime) or 0, locked == "true"
-end
+local lastMsg = 0
 
 sampRegisterChatCommand("srgn", function()
     enabled = not enabled
     if enabled then
-        sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} AKTIF. Tiktok And Yutube: @sion_299", -1)
-        lua_thread.create(function()
-            wait(10)
-            sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} MONETLOADER INI GRATIS!!!", -1)
-        end)
+        sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} AUTO REGEN AKTIF", -1)
     else
-        sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} MATI. Tiktok And Yutube: @sion_299", -1)
-        lua_thread.create(function()
-            wait(10)
-            sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} MONETLOADER INI GRATIS!!!", -1)
-        end)
+        sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} AUTO REGEN MATI", -1)
     end
 end)
 
@@ -66,72 +26,23 @@ end
 function main()
     repeat wait(0) until isSampAvailable()
 
-local lastTime, isLocked = loadLockData()
-local now = os.time()
-
-if isLocked then
-    thisScript():unload()
-    return
-end
-
-if now > expireTime then
-    saveLockData(now, true)
-    thisScript():unload()
-    return
-end
-
-if now < lastTime then
-    saveLockData(lastTime, true)
-    thisScript():unload()
-    return
-end
-
-saveLockData(now, false)
-
     sampAddChatMessage("{39C0FF}[Sion Auto Regen HP] {FFFFFF}Loaded! Ketik {39C0FF}/srgn", -1)
-    
-    lua_thread.create(function()
-        wait(10)
-        sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} MONETLOADER INI GRATIS!!!", -1)
-    end)
 
-while true do
-    wait(70)
-
-    local now = os.time()
-    local lastTime, isLocked = loadLockData()
-
-    if isLocked then
-        thisScript():unload()
-        return
-    end
-
-    if now > expireTime then
-        saveLockData(now, true)
-        thisScript():unload()
-        return
-    end
-
-    if now < lastTime then
-        saveLockData(lastTime, true)
-        thisScript():unload()
-        return
-    end
-
-    saveLockData(now, false)
+    while true do
+        wait(70)
 
         if enabled then
             local hp = getCharHealth(PLAYER_PED)
 
             if hp < 100 then
-    setCharHealth(PLAYER_PED, 100)
+                setCharHealth(PLAYER_PED, 100)
 
-    local now = os.clock()
-    if now - lastMsg > 1.5 then
-        sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} HP Di Regen. Tiktok And YouTube: @Sion_299", -1)
-        lastMsg = now
+                local now = os.clock()
+                if now - lastMsg > 1.5 then
+                    sampAddChatMessage("{39C0FF}[Sion]{FFFFFF} HP Di Regen. Tiktok & YouTube: @sion_299", -1)
+                    lastMsg = now
+                end
             end
-          end
         end
     end
 end
